b78410f1aa66d108caf292f3dab4a15029cc4697
RaxxCoin <raxxcoin@gmail.com>
2019-01-27 17:37:57 -0800

    add bank

--- test/simpleBank.test.js
+++ test/simpleBank.test.js
@@ -16,6 +16,8 @@ contract('SimpleBank', function(accounts) {
   const bob = accounts[2];
   const deposit = web3.utils.toBN(2);
 
+  console.log(`Alice: ${alice}`)
+
   it("mark addresses as enrolled", async () => {
     const bank = await SimpleBank.deployed();
 
@@ -37,36 +39,44 @@ contract('SimpleBank', function(accounts) {
     var result = await bank.deposit({from: alice, value: deposit});
     const balance = await bank.balance({from: alice});
 
-    assert.equal(deposit.toString(), balance, 'deposit amount incorrect, check deposit method');
+    assert.equal(deposit.toString(), balance,
+    'deposit amount incorrect, check deposit method');
 
     const expectedEventResult = {accountAddress: alice, amount: deposit};
 
     const logAccountAddress = result.logs[0].args.accountAddress;
     const logDepositAmount = result.logs[0].args.amount.toNumber();
 
-    assert.equal(expectedEventResult.accountAddress, logAccountAddress, "LogDepositMade event accountAddress property not emitted, check deposit method");
-    assert.equal(expectedEventResult.amount, logDepositAmount, "LogDepositMade event amount property not emitted, check deposit method");
+    assert.equal(expectedEventResult.accountAddress, logAccountAddress,
+      "LogDepositMade event accountAddress property not emitted, check deposit method");
+    assert.equal(expectedEventResult.amount, logDepositAmount,
+      "LogDepositMade event amount property not emitted, check deposit method");
   });
 
   it("should withdraw correct amount", async () => {
     const bank = await SimpleBank.deployed();
     const initialAmount = 0;
-   
-	var result = await bank.withdraw(deposit, {from: alice});
+
+	  var result = await bank.withdraw(deposit, {from: alice});
     const balance = await bank.balance({from: alice});
 
-    assert.equal(balance.toString(), initialAmount.toString(), 'balance incorrect after withdrawal, check withdraw method');
+    assert.equal(balance.toString(), initialAmount.toString(),
+     'balance incorrect after withdrawal, check withdraw method');
 
     const accountAddress = result.logs[0].args.accountAddress;
     const newBalance = result.logs[0].args.newBalance.toNumber();
     const withdrawAmount = result.logs[0].args.withdrawAmount.toNumber();
 
-    const expectedEventResult = {accountAddress: alice, newBalance: initialAmount, withdrawAmount: deposit};
+    const expectedEventResult = {accountAddress: alice, newBalance:
+      initialAmount, withdrawAmount: deposit};
 
 
-    assert.equal(expectedEventResult.accountAddress, accountAddress, "LogWithdrawal event accountAddress property not emitted, check deposit method");
-    assert.equal(expectedEventResult.newBalance, newBalance, "LogWithdrawal event newBalance property not emitted, check deposit method");
-    assert.equal(expectedEventResult.withdrawAmount, withdrawAmount, "LogWithdrawal event withdrawalAmount property not emitted, check deposit method");
+    assert.equal(expectedEventResult.accountAddress, accountAddress,
+      "LogWithdrawal event accountAddress property not emitted, check deposit method");
+    assert.equal(expectedEventResult.newBalance, newBalance,
+       "LogWithdrawal event newBalance property not emitted, check deposit method");
+    assert.equal(expectedEventResult.withdrawAmount, withdrawAmount,
+       "LogWithdrawal event withdrawalAmount property not emitted, check deposit method");
 
   });
 });
